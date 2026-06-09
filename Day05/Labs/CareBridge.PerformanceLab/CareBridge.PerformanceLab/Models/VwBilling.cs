752f87d1fe055c20f4589a753f23f2f0c7de805f
Navaneeth Siliveri <navaneethsiliveri7288@gmail.com>
2026-06-09 14:26:06 +0530

    Day05 - Building High-Performance Data Access Layers with EF Core

---
+++ Day05/Labs/CareBridge.PerformanceLab/CareBridge.PerformanceLab/Models/VwBilling.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CareBridge.PerformanceLab.Models;
+
+public partial class VwBilling
+{
+    public int Claimid { get; set; }
+
+    public decimal BilledAmount { get; set; }
+
+    public decimal? ReimbursedAmt { get; set; }
+
+    public string Status { get; set; } = null!;
+}
